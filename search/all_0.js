f1ba68bd61d35f084bf42db123c6667e170b981d
egor-tensin <Egor.Tensin@gmail.com>
2023-07-04 00:23:27 +0000

    Deploying to gh-pages from @ egor-tensin/winapi-utf8@5df6c58501383d82eff71e89e02957837258481c 🚀

---
+++ search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['narrow_0',['narrow',['../utf8_8hpp.html#ae643a038e8194c1f0d14bdf9edcafcb7',1,'winapi::narrow(const std::wstring &amp;)'],['../utf8_8hpp.html#a2795e1f7649c053417f39b90d3f3480f',1,'winapi::narrow(const std::u16string &amp;)'],['../utf8_8hpp.html#aeeef0f99233162d79ebedc551e87f0d0',1,'winapi::narrow(const void *src, std::size_t nb)'],['../utf8_8hpp.html#a46452a40203a7181d4f349ced5c0a584',1,'winapi::narrow(const std::vector&lt; T, Alloc &gt; &amp;src)']]]
+];
---
+++ search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['winapi_2dutf8_7',['winapi-utf8',['../index.html',1,'']]]
+];
