007110e1b6267a0d5f71f404ddb23bd3f07c454a
Egor Tensin <egor@tensin.name>
2026-09-21 23:10:26 +0500

    update my email

--- include/winapi/utf8.hpp
+++ include/winapi/utf8.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Egor Tensin <Egor.Tensin@gmail.com>
+// Copyright (c) 2020 Egor Tensin <egor@tensin.name>
 // This file is part of the "winapi-utf8" project.
 // For details, see https://github.com/egor-tensin/winapi-utf8.
 // Distributed under the MIT License.
