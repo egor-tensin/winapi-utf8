625fc10083872577816a8c5423e955eeb3c8b937
Egor Tensin <egor@tensin.name>
2026-09-21 23:10:44 +0500

    remove annoying trailing period from repo URLs

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2020 Egor Tensin <egor@tensin.name>
 // This file is part of the "winapi-utf8" project.
-// For details, see https://github.com/egor-tensin/winapi-utf8.
+// For details, see https://github.com/egor-tensin/winapi-utf8
 // Distributed under the MIT License.
 
 #define BOOST_TEST_MODULE winapi_utf8 tests
--- test/string.cpp
+++ test/string.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2020 Egor Tensin <egor@tensin.name>
 // This file is part of the "winapi-utf8" project.
-// For details, see https://github.com/egor-tensin/winapi-utf8.
+// For details, see https://github.com/egor-tensin/winapi-utf8
 // Distributed under the MIT License.
 
 #include <boost/test/unit_test.hpp>
